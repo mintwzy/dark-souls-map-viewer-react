950ba519fc5ff0c81696ddb3765e77355364ef8c
Wang Ziyi <mint.wzy@gmail.com>
2020-10-15 00:14:48 +0800

    [UPDATE] configure data path

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -19,20 +19,36 @@ class Viewer extends React.Component {
             vertexNumber: vertexNumber,
             data: null
         }
-        this.loadFile()
     }
 
-    loadFile = () => {
+    /*
+    The .iv file format stores a number of triangle meshes together in index array format.
+        First 4 bytes: uint32 containing number of chunks.<br>
+        Next 12 bytes: 3 float32s, use unknown.<br>
+        Next 16 * (number of chunks) bytes: 4 uint32s containing:
+
+         - Byte offset for start of vertex index data.
+         - Number of indices.
+         - Byte offset for start of vertex position data.
+         - Number of vertices.
+
+        Vertex index data, 2 * (number of indices) bytes: 1 uint16 per index.<br>
+        Vertex position data, 12 * (number of vertices) bytes: 3 float32s per vertex.
+    */
+    componentDidMount = () => {
         const request = new XMLHttpRequest();
-        request.open("GET", "../data/ds1/Undead Burg.iv", true)
-        request.responseType = "arraybuffer";
-        request.send()
         request.onreadystatechange = () => {
             if(request.readyState === 4) {
-                console.log(request.response)
                 let data = new Uint8Array(request.response)
+
+                // First 4 bytes: uint32 containing number of chunks
+                const num_models = new Uint32Array(data.buffer.slice(0, 4))[0];
+                const chunks = Array.apply(null, {length: num_models}).map(Number.call, Number);
             }
         }
+        request.open("GET", "data/ds1/Undead Burg.iv", true)
+        request.responseType = "arraybuffer";
+        request.send()
     }
 
 
